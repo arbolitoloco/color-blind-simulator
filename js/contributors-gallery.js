cf9a4a7c23db7d851ae5887a0b77d7e6ed8f601a
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-10-01 22:24:12 -0300

    Add script to build contributors cards dinamically. Fix constributors-gallery name in styles.css to match id in index.html

---
+++ js/contributors-gallery.js
@@ -0,0 +1,35 @@
+{/* <div id="contributor-gallery">
+      <div class="contributor">
+        <img src="https://avatars0.githubusercontent.com/u/17555247?v=4" alt="arbolitoloco" />
+        <strong>arbolitoloco</strong>
+        <a href="https://github.com/arbolitoloco">github.com/arbolitoloco</a>
+      </div>
+    </div> */}
+
+const contributorGallery = document.getElementById('contributors-gallery');
+
+const buildContributorCard = (contributor) => {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const strong = document.createElement('strong');
+  const a = document.createElement('a');
+
+  contributorGallery.append(div);
+  div.append(img);
+  div.append(strong);
+  div.append(a);
+
+  div.setAttribute('class', 'contributor');
+  img.setAttribute('src', contributor.avatar_url);
+  img.setAttribute('alt', contributor.login);
+  strong.innerHTML = contributor.login;
+  a.innerHTML = `github.com/${contributor.login}`;
+  a.setAttribute('href', contributor.html_url);
+}
+
+fetch('https://api.github.com/repos/arbolitoloco/color-blind-simulator/contributors')
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((contributor) => buildContributorCard(contributor));
+    })
+    .catch((err) => console.warn('Problem retrieving contributors.', err));
